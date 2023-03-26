efc38d7546eb6fd671fa7cc6b94f7f585ef188bc
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2023-03-26 13:48:28 +0530

    Put operation implemented

--- helloworldservice/src/main/java/com/example/helloworldservice/Student/StudentController.java
+++ helloworldservice/src/main/java/com/example/helloworldservice/Student/StudentController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping(path = "/student")
@@ -35,8 +39,15 @@ public class StudentController {
     @DeleteMapping(path= "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentid){
         studentService.deleteStudent(studentid);
-
     }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+        @PathVariable("studentId") Long studentId,
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String email) {
+            studentService.updateStudent(studentId, name, email);
+        }
 
 	
     
--- helloworldservice/src/main/java/com/example/helloworldservice/Student/StudentService.java
+++ helloworldservice/src/main/java/com/example/helloworldservice/Student/StudentService.java
@@ -1,10 +1,14 @@
 package com.example.helloworldservice.Student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ch.qos.logback.core.joran.conditional.IfAction;
+import jakarta.transaction.Transactional;
 
 @Service
 public class StudentService {
@@ -37,5 +41,25 @@ private static StudentRepository studentRepository;
 			throw new IllegalStateException("student with id" +studentid +"does not exists");
 		}
 		studentRepository.deleteById(studentid);
+	}
+
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+
+		Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(
+			"student with id" + studentId + "does not exist"));
+
+			if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+				student.setName(name);
+			}
+
+			if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+				Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+				if(studentOptional.isPresent()) {
+					throw new IllegalStateException("email already present");
+				}
+				student.setEmail(email);
+			}
 	}
 }
