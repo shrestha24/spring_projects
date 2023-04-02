8999ca2a65a75af04a2517bf3a33742949ffb3c8
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2023-04-02 11:29:08 +0530

    Spring Security implemented initial commit for basic login page

---
+++ helloworldservice/src/main/java/com/example/helloworldservice/Student/HomeResourceController.java
@@ -0,0 +1,15 @@
+package com.example.helloworldservice.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResourceController {
+
+    @GetMapping("/")
+    public String home(){
+        return("<h1>Welcome</h1>");
+    }
+    
+}
+ 
