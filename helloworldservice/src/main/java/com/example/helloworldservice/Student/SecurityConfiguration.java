0ace099f8f0ecd4a8cf4fcd7850441d6b5f5dca7
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2023-04-04 23:31:44 +0530

    Final commit

---
+++ helloworldservice/src/main/java/com/example/helloworldservice/Student/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.helloworldservice.Student;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+}
